6d66994d2d0d4a68be0e15bfa23ffc6344053289
T1nker-1220 <marquezjohnnathanieljade@gmail.com>
2024-09-30 23:39:19 +0800

    update.py

--- main.py
+++ main.py
@@ -1,1 +1,40 @@
-print("Hello WOrld!")
+import requests
+
+api_key = "/vTftsNwDaweVZNqQLyePw==nYcCW3NVHxLYnUNw"  
+
+latitude = "14.599512" 
+longitude = "120.984222"  
+
+
+complete_url = f"https://api.api-ninjas.com/v1/weather?lon={longitude}&lat={latitude}"
+
+
+headers = {
+    'X-Api-Key': "/vTftsNwDaweVZNqQLyePw==nYcCW3NVHxLYnUNw"  
+}
+
+
+response = requests.get(complete_url, headers=headers)
+
+
+if response.status_code == 200:
+    data = response.json()
+    
+    
+    print("Response Data:", data)
+
+    temp = data.get('temp', 'No temperature data available')
+    humidity = data.get('humidity', 'No humidity data available')
+    
+    
+    if 'weather' in data and isinstance(data['weather'], list) and len(data['weather']) > 0:
+        description = data['weather'][0]['description']
+    else:
+        description = "No weather description available"
+
+    
+    print(f"Temperature: {temp}°C")
+    print(f"Humidity: {humidity}%")
+    print(f"Weather: {description}")
+else:
+    print(f"Error fetching data: {response.status_code}, {response.text}")
